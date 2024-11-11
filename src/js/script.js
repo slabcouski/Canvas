7f3cc6479009a8aeace4fa880a86db543251931b
slabcouski <prof.marcelo.garcia@gmail.com>
2024-11-11 12:23:27 -0300

    decimo commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+$(document).ready(function(){
+    // Smooth scrolling
+$('a[href^="#"]').click(function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+    });
+});
